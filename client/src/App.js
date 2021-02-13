2981471037a571d0a096ebd2712cad7673311d23
Marlee Eyre <marlee.eyre@gmail.com>
2021-02-13 13:58:59 -0500

    import questioscard

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+
+import QuestionCard from './components/QuestionCard';
+//import QuestionForm from './components/QuestionForm';
+
+import store from './store';
+
+ class App extends Component {
+   render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+         <QuestionCard />
+        </div>
+      </Provider>
+    );
+  }
+   }
+  
 
 export default App;
