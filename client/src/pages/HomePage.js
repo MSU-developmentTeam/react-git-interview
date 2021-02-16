daae9b1a2a75e2809754d3ee586fc5ad8a2360d4
Sam Bishop <sambishop3015@hotmail.com>
2021-02-16 18:14:29 -0500

    Homepage Layout

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../components/Header";
+import QuestionCard from "../components/QuestionCard";
+// import Footer from "../components/Footer";
+import { connect } from 'react-redux'
+
+export const HomePage = (props) => {
+    return (
+        <div>
+            <Header />
+            <QuestionCard />
+            {/* <Footer /> */}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+// const mapDispatchToProps = {
+    
+// }
+
+export default connect(mapStateToProps /*mapDispatchToProps*/)(HomePage)
