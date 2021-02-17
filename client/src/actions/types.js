2f126c05063d5e2c2c77cfdd4620377c2fc26419
Alex Tatum <tatum_alex@ymail.com>
2021-02-16 19:48:59 -0500

    added signup and login actions

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
 export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
 export const NEW_QUESTION = 'NEW_QUESTION';
-export const FETCH_LOGIN = 'FETCH_LOGIN';
-export const NEW_SIGNUP = 'NEW_SIGNUP';
+export const FETCH_USER = 'FETCH_USER';
+export const NEW_USER = 'NEW_USER';
