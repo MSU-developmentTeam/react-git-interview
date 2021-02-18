8384a731e4fa0962bdf55a30fde7cae11d2099e0
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-18 14:19:55 -0500

    imported Footer to Homepage & added icons style

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import QuestionCard from "../components/QuestionCard";
-// import Footer from "../components/Footer";
+ import Footer from "../components/Footer";
 import { connect } from 'react-redux'
 
 export const HomePage = (props) => {
@@ -9,7 +9,7 @@ export const HomePage = (props) => {
         <div>
             <Header />
             <QuestionCard />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
