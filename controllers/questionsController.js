b90cc0216d0dafa2c6412cd348201260e69db4d2
Muneeb Ali <muneebbwp@gmail.com>
2021-02-19 20:03:09 -0600

    we added question controller

---
+++ controllers/questionsController.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const mongoose = require("mongoose");
+
+module.exports = {
+  findAll: function (req, res) {
+    db.Question.findAll(req.query).then((dbModel) => res.json(dbModel));
+  },
+
+  findByTopic: function (req, res) {
+    db.Question.findByTopic(req.params.topic).then((dbModel) =>
+      res.json(dbModel)
+    );
+  },
+
+  findById: function (req, res) {
+    db.Question.findById(req.params.id).then((dbModel) => res.json(dbModel));
+  },
+
+  create: function (req, res) {
+    db.Question.create({
+      UserId: req.user.id,
+      topic: req.body.topic,
+      body: req.body.body,
+      answer: req.body.answer,
+    }).then((dbModel) => res.json(dbModel));
+  },
+
+  remove: function (req, res) {
+    db.Question.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel));
+  },
+
+  update: function (req, res) {
+    db.Question.findById({ _id: req.params.id }, req.body).then((dbModel) =>
+      res.json(dbModel)
+    );
+  },
+};
