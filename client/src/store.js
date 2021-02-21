bff69e2ca9ecd5ef7e0801fba98ef4daf774dc97
progg1992 <roggpierce@gmail.com>
2021-02-20 23:55:12 -0600

    updated store

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialStae, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 // possibly need logger middleware
 // https://jasonwatmore.com/post/2017/09/16/react-redux-user-registration-and-login-tutorial-example#user-actions-js
