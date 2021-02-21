58ea3a2942ca27675a6f210133ff3e523cd5421e
progg1992 <roggpierce@gmail.com>
2021-02-20 23:49:01 -0600

    added a new header and question card to try to get data and display it

---
+++ client/src/components/QuestionCard2.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getQuestions, deleteQuestion } from '../actions/questionActions';
+import PropTypes from 'prop-types';
+import '../styles/styles.css';
+
+class QuestionTwo extends Component {
+    static propTypes = {
+        getQuestions: PropTypes.func.isRequired,
+        question: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
+
+    const 
+
+    componentDidMount() {
+        this.props.getQuestions();
+    }
+
+    onDeleteClick = id => {
+        this.props.deleteQuestion(id)
+    }
+    render() {
+        const { questions } = this.props.question;
+
+        return (
+            <Container>
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {questions.map(({ _id, topic, body, answer, keyWords }) => (
+                            <CSSTransition key={_id} timeout={500} className='fade'>
+                                <ListGroupItem>
+                                    {this.propTypes.isAuthenticated ? <Button className='remove-btn' color='danger' size='sm' onClick={this.onDeleteClick.bind(this, _id)}>&times;</Button> : null}
+                                    {topic, body, answer, keyWords}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    question: state.question,
+})
+
+export default connect(mapStateToProps, { getQuestions, deleteQuestion })(QuestionTwo);
