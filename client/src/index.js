b72b9e66f0d68fce7560c1ec2d18022b0074a726
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-21 20:55:08 -0500

    removed react strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
