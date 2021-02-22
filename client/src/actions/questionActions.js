2cbf3e05b3768bb3226ac1e72a9e6d8b5d47e040
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-21 20:53:38 -0500

    added search actions

--- client/src/actions/questionActions.js
+++ client/src/actions/questionActions.js
@@ -1,4 +1,4 @@
-import { GET_QUESTIONS, ADD_QUESTION, DELETE_QUESTION, QUESTIONS_LOADING } from './types';
+import { GET_QUESTIONS, ADD_QUESTION, DELETE_QUESTION, QUESTIONS_LOADING, SEARCH_QUESTIONS } from './types';
 import { tokenConfig } from './userActions';
 import { returnErrors } from './errorActions';
 import axios from 'axios';
@@ -17,15 +17,15 @@ export const getQuestions = () => dispatch => {
 };
 
 export const addQuestion = question =>
-     (dispatch, getState) => {
-    axios.post('api/questions', question, tokenConfig(getState))
-        .then(res =>
-            dispatch({
-                type: ADD_QUESTION,
-                payload: JSON.parse(res.data)
-            })
-        ).catch(err => dispatch(returnErrors(err.res.data, err.res.status)));
-};
+    (dispatch, getState) => {
+        axios.post('api/questions', question, tokenConfig(getState))
+            .then(res =>
+                dispatch({
+                    type: ADD_QUESTION,
+                    payload: JSON.parse(res.data)
+                })
+            ).catch(err => dispatch(returnErrors(err.res.data, err.res.status)));
+    };
 
 export const deleteQuestion = id => (dispatch, getState) => {
     axios.delete(`api/questions/${id}`, tokenConfig(getState)).then(res =>
@@ -42,6 +42,16 @@ export const setQuestionsLoading = () => {
         type: QUESTIONS_LOADING
     }
 };
+
+export const searchingQuestion = topic => (dispatch, getState) => {
+    axios.get(`api/questions/${topic}`, tokenConfig(getState)).then(res => {
+        console.log(res);
+        return {
+            type: SEARCH_QUESTIONS,
+            payload: topic
+        }
+    }).catch(err => dispatch(returnErrors(err.res.data, err.res.status)));
+}
 
 //TODO
 //export function for all question categories from DB
