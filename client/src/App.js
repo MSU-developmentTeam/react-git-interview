d34c9a7ae3e23694041a51be2d50e1fc3d697efc
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-23 11:32:46 -0500

    commented unnecessary imports

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import './styles/styles.css'
 //import QuestionForm from './components/QuestionForm';
 
 import store from './store';
-import LoginModal from './components/auth/loginModal';
-import RegisterModal from './components/auth/registerModal';
+//import LoginModal from './components/auth/loginModal';
+//import RegisterModal from './components/auth/registerModal';
 
  class App extends Component {
    render(){
