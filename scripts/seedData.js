12d933efce21d268128b50b37109a93da22f4897
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-23 11:34:34 -0500

    added space in keywords data

--- scripts/seedData.js
+++ scripts/seedData.js
@@ -11,14 +11,14 @@ const questionSeed = [
         topic: "HTML",
         body: "Why are you interested in using HTML to build websites?",
         answer: "Web-building tools are great for laymen and professional developers, but I think it’s important to understand the underlying technology so I have more control over how sites look and behave. For instance, when I use WordPress to build a site, I often find that I can get better results by inserting my own HTML instead of relying on the provided tools",
-        keyWords: ["HTML", "HTML5"],
+        keyWords: [" HTML ", " HTML5 "],
         date: new Date(Date.now())
     },
     {
         topic: "JavaScript",
         body: "How can you convert the string of any base to integer in JavaScript?",
         answer: "The parseInt() function is used to convert numbers between different bases parseInt() takes the string to be converted as its first parameter, and the second parameter is the base of the given string.",
-        keyWords: ["JavaScript", "parseInt()", "parse"],
+        keyWords: [" JavaScript ", " parseInt() ", " parse "],
         date: new Date(Date.now())
     }
 
