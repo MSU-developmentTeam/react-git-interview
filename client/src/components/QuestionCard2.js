966decaca8048a7b5e4bb578337747097f777bc3
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-23 11:33:38 -0500

    added cards for questions

--- client/src/components/QuestionCard2.js
+++ client/src/components/QuestionCard2.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Container } from 'react-bootstrap';
-//import { TransitionGroup } from 'react-transition-group';
+import { Card } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { getQuestions, deleteQuestion } from '../actions/questionActions';
 import PropTypes from 'prop-types';
@@ -26,20 +26,27 @@ class QuestionTwo extends Component {
     }
 
 
-
     render() {
         const { questions } = this.props.question;
         console.log(this.props)
         return (
             <Container>
                 <div>
-                    {/* <TransitionGroup className="shopping-list"> */}
-                        {questions.map(({_id, topic, body, answer, keyWords }) => (
-                            <p key={ _id}>
-                               { topic}{body}{answer}{keyWords}
-                            </p>
-                        ))}
-                    {/* </TransitionGroup> */}
+                    {questions.map(({ _id, topic, body, answer, keyWords }) => (
+                        <Card key={_id}>
+                            <Card.Header> Question </Card.Header>
+                            <Card.Body >
+                                <Card.Title >
+                                    {topic}
+                                </Card.Title>
+                                <Card.Text>
+                                    {body}
+                                </Card.Text>
+                                <Card.Text>{answer}</Card.Text>
+                                <Card.Footer>{keyWords}</Card.Footer>
+                            </Card.Body>
+                        </Card>
+                    ))}
                 </div>
             </Container>
         )
@@ -48,6 +55,6 @@ class QuestionTwo extends Component {
 
 const mapStateToProps = (state) => ({
     question: state.question,
-})
+});
 
 export default connect(mapStateToProps, { getQuestions, deleteQuestion })(QuestionTwo);
