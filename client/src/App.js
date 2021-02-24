5b718b0fd46df07a3ffff7f4a5542d4c1d73c536
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-24 12:56:15 -0500

    cleaned up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,29 @@
-
 import './App.css';
 import { Provider } from 'react-redux';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
-//import LoginForm from './components/LoginForm';
-//import SignupForm from './components/SignupForm';
 import './styles/styles.css'
-//import QuestionForm from './components/QuestionForm';
-
 import store from './store';
-//import LoginModal from './components/auth/loginModal';
-//import RegisterModal from './components/auth/registerModal';
 
- class App extends Component {
-   render(){
+
+class App extends Component {
+  render() {
     return (
       <Provider store={store}>
         <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/" exact>
-              <HomePage />
-            </Route> 
-          </Switch>
-        </div>
+          <div className="App">
+            <Switch>
+              <Route path="/" exact>
+                <HomePage />
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </Provider>
     );
   }
-   }
-  
+}
+
 
 export default App;
