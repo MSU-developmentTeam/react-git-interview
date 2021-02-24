fa105cd64ab9f5be483e59e83ed20734512d7e62
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-24 12:56:40 -0500

    added some sample data to search by topic

--- client/src/data.js
+++ client/src/data.js
@@ -1,1 +1,15 @@
 //typeahead keywords
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    { topic: 'JavaScript'},
+    { topic: 'HTML'},
+    { topic: 'CSS'},
+    { topic: 'Node'},
+    { topic: 'SQL'},
+    { topic: 'MongoDB'},
+    { topic: 'Sequelize'},
+    { topic: 'Mongoose'},
+    { topic: 'API'},
+    { topic: 'RESTful services'},
+    { topic: 'jQuery'}
+];
