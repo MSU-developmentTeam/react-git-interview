b494d6dc94bc85e25e7c3ef09d9f638a0dbb0217
Sam Bishop <sambishop3015@hotmail.com>
2021-02-27 14:27:34 -0500

    Modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import './styles/styles.css'
 import store from './store';
-
+//import LandingPage from "./pages/LandingPage";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Router>
-          <div className="App">
+          <div className="App" >
             <Switch>
               <Route path="/" exact>
                 <HomePage />
+                {/* <LandingPage /> */}
               </Route>
             </Switch>
           </div>
