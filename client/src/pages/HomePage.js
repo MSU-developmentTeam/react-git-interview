9d5387bb4433f93f42d0861efa29c91aaee659d2
Sam Bishop <sambishop3015@hotmail.com>
2021-02-27 14:28:46 -0500

    Added Landing Component

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,11 +3,13 @@ import Header from '../components/Header';
 import QuestionCard from '../components/QuestionCard';
 import Footer from "../components/Footer";
 import { connect } from 'react-redux'
+import LandingPage from "../components/LandingComponent";
 
 export const HomePage = (props) => {
     return (
         <div>
             <Header />
+            <LandingPage />
             <QuestionCard />
             <Footer />
         </div>
