92878e49a5074c9ea683a8e378d850af442da3d2
progg1992 <roggpierce@gmail.com>
2021-02-26 19:39:35 -0600

    changed from an array of objects to strings

--- client/src/data.js
+++ client/src/data.js
@@ -1,15 +1,3 @@
 //typeahead keywords
 // eslint-disable-next-line import/no-anonymous-default-export
-export default [
-    { topic: 'JavaScript'},
-    { topic: 'HTML'},
-    { topic: 'CSS'},
-    { topic: 'Node'},
-    { topic: 'SQL'},
-    { topic: 'MongoDB'},
-    { topic: 'Sequelize'},
-    { topic: 'Mongoose'},
-    { topic: 'API'},
-    { topic: 'RESTful services'},
-    { topic: 'jQuery'}
-];
+export default [ 'JavaScript', 'HTML', 'CSS', 'Node'];
